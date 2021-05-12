b9d2aaf32917bb6004a051d9131a9ad8d97ef012
janardhanreddy <janardhanreddy@in.ibm.com>
2021-05-12 12:15:41 +0530

    first commit

---
+++ main1.tf
@@ -0,0 +1,87 @@
+locals {
+   root_private_key = <<EOT
+-----BEGIN RSA PRIVATE KEY-----
+MIIJKAIBAAKCAgEAtdP2o92/fUQeccYiEBk51Z1FfqW7+AQBpo0XxO10hCFA/9sj
+uztKoSu3IH8x2dDhm2ESSo+W4bt2dqwBa9oj4NJsizJXQXXlWMamlG3ga9Rb9fJ3
+oZX/sJZFQNP6jQX1yc/lQ3B3B68tKbdo/PTUazQhdpu+ZdSWA0q0rZU6z6RuzfBO
+1uqiL8BtM835ghmqD/uapDK9PlvvHUwU8hbduPUCvbf14qiqvQVmCAj7ZSQERkIK
+wfsQeEgJUvmUeKD8cL+uWAecPm5amc2TcffC272ufVzV+n6E/V1tG5sbDkxNd1bH
+5DDS/4yqjpQvQDy1i7ZZMiWpvXcMmYWIHJ1llU6ddkuLAK0UkwkhOM1Ho2ogf5Rb
+B2eAJ90L+LbgYHqmIhiBCMurRSfya+7f+QNoq5KzgL5gEWs2EqCqXjYG1jcCl7Rr
+XmOsjCRfheZ2vXxmDLchMr22uhhXCexSYD6k2jejUG9cUkkyG7G3i3blVRArXn9f
+1Eqtw+slWRqUMXGtu9qpvqTEsW96rniWemtjzen1d/1KBoOrjcMjy1iXQ2XTE/wr
+twNY9SiFmoLeI1B1dxRfQD9maf9EJm7Izzu5m4zqgyPPL0m4e8JZC33PrFGuFFTS
+ZSO3i5IbQPxEJQE6piWQroacTdmD60K8Hj2NgeJIRyJc+EbGzdOtrW24SwMCAwEA
+AQKCAgArOzBjGbATuoR6jJ9AsJ69mV0SlZkxiNJTpSxHmCWh3L41lwbR13ERDPHH
+ejaCEVXyuYu9ypIXLZkm8hKMcBsjsyQ3nEp+t+dFykWE+PLo8F7FBVdIRf+7Hp9s
+oHnr2cTsrM9Xz3+dhhr41jDSI+v1aTmjl7gXQgpA0hxPQTHweKoXsNEt7C+PpvLP
+5GM0eaGjlzDXZXD1SKp7jsFUJfelqCt0rC58K5ocwxHVSz1qeLvDcKRaJrVtw8Lj
+xY91duFOdWSgbX7KoTZNZ0FtUXk1Ac5g5BfELRy1mOb+L0ypg57PhwSTzr/8HpfW
+uojZfxWzGDHw6F+JOqr2PXd1Rj8GcEdlrReVb+FhJGQcejz71/mQycptMOF5k/Bu
+0VKIIwtEWwBfUTdn++cHUW/hv48B5aJsALBfgUTSyG8aElIvj9jDqZJC3xcihIey
+8hYKZKxQP9OEab9BJejPexlwrJNla89Iw11ouHgau6T1irAiTYuMhKafigdK8IUV
+Mtj2zcGjbfAFhyv4RSLkdUhu6foY5GvJ8KuoLsmFDNaJ7u4zpmmhqyRYt0icFVpA
+lPdIu1X7gMadyattkvNyx2ddx47/gnRischF8xbSHzwtvlizTiXtQERwBm/gZc3I
+E9uIvSn7lA5khdvENP2ivXQWH8+mQFmv3zKHCVEVan282Y7h8QKCAQEA2bcYFXOr
+dH7c2vRQIZY3w0jtgPQAVikHvomFHdYtsnjUAl4bqDPlE92umMCP1RMYcc6E4bIF
+Sp3J5DXPQUdB26Crid3yOu6sEKgNIdrily4EPJUIZWgFASNWfw1K6rGWCiWTzeB9
+dGq62J/36hTzSyF7SXpWrRDdI2sRa1PGXQdpwXT5pgpcic61vwGkXH5NkUQPEWoM
+HZVBB+nEcbQqDLBZ760vtSglFyuNHGe8JDzyTKo3UBf7GPtEU4FvomEP8y+r47PD
++LfLWY+8YcZ3w1hMhlhNNGDUyMOcaNwyc1f48cwSGv0LYL/E06zDXQeSKtN6LeD6
+9h3kpJxB5VUmPQKCAQEA1c1UpDY+dX2s4p0B7t1RajpdBR83P+PI7CuYI/Ful1vw
+5UOVFa/ObatXspl136A87ifjuccuTlQRteKxCir2eMCpU6BwtEqU3p0pJ9M2sKqp
+OEaiD27UBi6VwdUSmbUhjtbOfr/hOIYAzpUb4q2qyVAKC8DRS54RiDZCband4EyI
+ML3lgVktrcsYXV2yTS3wnXzDxHOQbE1fhT89IITAwHRQGbxnPd8BdGGtNCAnlNz2
+GKsaNrARaT3Y5GcgypZ/rpBWA/XVZNYmgGgZBBMqyHlorZ+lV9ztTcquouvuEvNC
+BHPNeVG+3H5+pAwLEMvCcA5Pv/hSa8QbCX3t7TOKPwKCAQAG0fKCMYZsdtDqVv7v
+nmoOxwkRohMkn9JrSJmP7ratwZU6ocISCdYiQi6HAHlA/oEkh4ioHVCHHtxFNt5Q
+ODA69hSJlBdTe0iOmQUYS2gPvlRJpY2Gx7Robbf28O6o3XlKxhSUPXUQoYtK3+//
+ODrK3rii+58desrCokcAarZrEaFhew2bAvYVJxGQcze34gkCBPTH4d+RSayo18zc
+lv0+OoQGuxEkhqZqzGV2P1WoePRKLIbhDm9CQBTK3OInXw0F3DdxQZ9g+jj3jEYl
+dfcoemqjRr7cZjAkPeR3cHZwIJBe2rqyd7zUgg1jaCUSk/sjNk68gzC5ZwRTl8WE
+rp8tAoIBAQDLkkuSY7fK5RP1XevWRVLc459oXhWcNsY74TJmnt5fLgLMOTI9yPyz
+vZ6uBqKOgCMj3EVfwcBcgn2ugp9xgADYbxGK98v4Tr86y8fBDMIZKxYe6FosealW
+DYs4WSnxwt6mA+/wDcFjsWwZRy6bAiMItbYrBLgD2Qj2ejt78W66wAQ47McXBxOQ
+VD3pwGoiRKjDO2WnqyWLiYx28gTA7sYdOWJ/br2a0riu2MYzSJ6HhnCoglpeUfzy
+CRp4N52oZ+5BB1tvP+aksadNQlD3aUymP7uk/cpQQ6KMHH110VEJKCfm12CAsI4b
+ojHw/yqWHn0PdwEGV2obBEl+GzKtVRK/AoIBAFQM1RyehdFmi+mDwR/jEYymklkF
+m8YF5OScKEyxe9tDMebxLYM+5MLa44/llRBt/au0Zt5krxKSP0bQtWQBXuJIz3Go
+xrjd+7tZ4KDgDVhdIVvQZ9Ru57I50o0/bMxBYZEgvqlbfvMn36X3GdZonhG4oQ/s
+TrrYCR+Z13R7pLUDvbcYwzkUp++W0vxJ7pCL4rtpb7/pegsh2E0zy/pK80Knrt/7
+3tENi3NLjsFjy/ndAVp3Tli6BDGUHisvrbQ3IYxtcXVQ40FoH8n5n/pyFwy8FR4y
+Iz5AfG/47iRHKTSKm45hUUiuzW6EykLqObp3HkBCQnXKe6b+zNTWe/LxptY=
+-----END RSA PRIVATE KEY-----
+EOT
+}
+resource "null_resource" "winrm" {
+  provisioner "ansible" {
+    plays {
+      playbook {
+        file_path  = "winrm.yml"
+      }
+      inventory_file = "inventory.yml"
+      verbose        = true
+      forks          = 15
+      extra_vars = {
+        # Hosts
+        "service_hosts" = "windows_servers"
+        # Ansible
+        "ansible_user"                         = "administrator"
+        "ansible_password"                     = "0v9ketFl6p4334zzWlY2"
+        "ansible_connection"                   = "winrm"
+        "ansible_winrm_server_cert_validation" = "ignore"
+      }
+    }
+    ansible_ssh_settings {
+      insecure_no_strict_host_key_checking = true
+      connect_timeout_seconds              = 120
+      connection_attempts                  = 3
+    }
+  }
+  connection {
+    type        = "ssh"
+    user        = "root"
+    host        = "0.0.0.0"
+    private_key = local.root_private_key
+  }
+}
